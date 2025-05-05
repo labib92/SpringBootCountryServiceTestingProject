8f6c844e785ae36bc7bcd32950a468d17a529049
labib92 <labib_saleh_92@yahoo.com>
2025-05-05 15:24:30 +0300

    Created unit test for CountryService using Junit5 and Mockito frameworks.

--- src/main/java/com/countryservice/demo/services/CountryService.java
+++ src/main/java/com/countryservice/demo/services/CountryService.java
@@ -25,7 +25,15 @@ public class CountryService {
     }
 
     public Country getCountryByID(int id){
-       return countryRepository.findById(id).get();
+        List<Country> countries = countryRepository.findAll();
+        Country country = null;
+        for(Country i : countries){
+            if(i.getId() == id){
+                country = i;
+                break;
+            }
+        }
+        return country;
     }
 
     public Country getCountryByName(String countryName){
